734322b69a999f968dbe3a8eb9017cbae5e35f5d
AD-Archer <adarcher21@gmail.com>
2024-12-11 14:52:28 -0500

    Minor fixes, and changes made to prepare for the future updates

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 //imports
 import express from "express";
 import 'dotenv/config';
-import sequelize from "./config/dbconn.js";
 import taskRoutes from "./routes/tasks.js"
 import './models/testData.js';
 
@@ -13,19 +12,37 @@ const port = '3000';
 const url = "localhost"
 
 
-
 // app.use
 app.use(express.json());
-
 app.use('/tasks', taskRoutes)
 
 
-// routes using express
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
+
+/*
+// app.use(express.static('public')); 
+
+//tell express where to find our template files
+// app.use('view engine', 'ejs')
+// app.use("views", "./views")
+
+//tell express where to find our public files
+// app.use(express.static(join(process.cwd(), 'public')));
 
 
+
+// Start our server
+const startServer = async () => {
+    try {
+        await app.listen(3000);
+        console.log(`Website running at http://${url}:${port}`);
+    } catch (error) {
+        console.log('Oops! Server start failed:', error);
+    }
+};
+
+startServer();
+
+*/
 
  app.listen(port, () => {
     console.log(`Server is running on http://${url}:${port}`);
